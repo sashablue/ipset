22f269468c5fecfe87414e2bea92c2557c03ccf7
Sasha Blue <mailsashablue@gmail.com>
2025-03-18 23:36:08 +0100

    Update ipset-start.sh

--- ipset-start.sh
+++ ipset-start.sh
@@ -1,5 +1,6 @@
 # https://linux-audit.com/networking/iptables/blocking-ip-addresses-in-linux-with-iptables/
-git pull
-ipset -exist -file ipset.conf restore
+IPSETCONF=~/ipset/ipset.conf
+curl -s https://raw.githubusercontent.com/sashablue/ipset/refs/heads/master/ipset.conf -o $IPSETCONF
+ipset -exist -file $IPSETCONF restore
 iptables -I INPUT -m set --match-set blocklist src -j DROP
 iptables -I FORWARD -m set --match-set blocklist src -j DROP
